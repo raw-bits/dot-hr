8da4502b2a30dcd0a5f24d22f559e92bd608c30e
Rato Kuzmanić <rato.kuzmanic@live.com>
2022-02-14 23:30:03 +0100

    Add webpage

---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+require(`dotenv`).config()
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
+    siteTitle: `RAW BITS`,
+    siteTitleAlt: `RAW BITS`,
+    siteHeadline: `RAW BITS`,
+    siteUrl: `https://rawbits.hr`,
+    siteDescription: `We help clients design, implement, and maintain complex web solutions.`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `RAW BITS`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `RAW BITS`,
+        short_name: `RAW BITS`,
+        description: `We help clients design, implement, and maintain complex web solutions.`,
+        start_url: `/`,
+        background_color: `#141821`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
